b910d77c57381f8f3205ce35f005f8ce1a982426
Niklas Eicker <nikle1@gmx.de>
2017-05-14 15:54:17 +0200

    save money and token rewards

--- src/me/nikl/sudoku/GameRules.java
+++ src/me/nikl/sudoku/GameRules.java
@@ -16,45 +16,14 @@ public class GameRules {
     private double cost;
     private boolean saveStats;
     private String key;
+    private int moneyReward, tokenReward;
 
-    private Map<Integer, Double> moneyRewards;
-    private Map<Integer, Integer> tokenRewards;
-
-    public GameRules(Main plugin, String key, double cost, boolean saveStats){
+    public GameRules(String key, double cost, int moneyReward, int tokenReward, boolean saveStats){
         this.cost = cost;
         this.saveStats = saveStats;
         this.key = key;
-
-        loadRewards(plugin);
-    }
-
-
-    private void loadRewards(Main plugin) {
-        moneyRewards = new HashMap<>();
-        tokenRewards = new HashMap<>();
-
-        if(!plugin.getConfig().isConfigurationSection("gameBox.gameButtons." + key + ".scoreIntervals")) return;
-
-        ConfigurationSection onGameEnd = plugin.getConfig().getConfigurationSection("gameBox.gameButtons." + key + ".scoreIntervals");
-        for (String key : onGameEnd.getKeys(false)) {
-            int keyInt;
-            try {
-                keyInt = Integer.parseInt(key);
-            } catch (NumberFormatException e) {
-                Bukkit.getLogger().warning(plugin.lang.PREFIX + " NumberFormatException while getting the rewards from config!");
-                continue;
-            }
-            if (onGameEnd.isSet(key + ".money") && (onGameEnd.isDouble(key + ".money") || onGameEnd.isInt(key + ".money"))) {
-                moneyRewards.put(keyInt, onGameEnd.getDouble(key + ".money"));
-            } else {
-                moneyRewards.put(keyInt, 0.);
-            }
-            if (onGameEnd.isSet(key + ".tokens") && (onGameEnd.isDouble(key + ".tokens") || onGameEnd.isInt(key + ".tokens"))) {
-                tokenRewards.put(keyInt, onGameEnd.getInt(key + ".tokens"));
-            } else {
-                tokenRewards.put(keyInt, 0);
-            }
-        }
+        this.moneyReward = moneyReward;
+        this.tokenReward = tokenReward;
     }
 
     public double getCost() {
@@ -69,11 +38,11 @@ public class GameRules {
         return key;
     }
 
-    public Map<Integer,Double> getMoneyRewards() {
-        return moneyRewards;
+    public int getMoneyReward() {
+        return moneyReward;
     }
 
-    public Map<Integer,Integer> getTokenRewards() {
-        return tokenRewards;
+    public int getTokenReward() {
+        return tokenReward;
     }
 }
