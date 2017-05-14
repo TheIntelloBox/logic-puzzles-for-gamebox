aa818523ddabd7a2a5b4620f86a728f7967f5341
Niklas Eicker <nikle1@gmx.de>
2017-05-14 15:53:20 +0200

    load items on enable. Get a random puzzle on Game start

--- src/me/nikl/sudoku/GameManager.java
+++ src/me/nikl/sudoku/GameManager.java
@@ -4,15 +4,20 @@ import me.nikl.gamebox.GameBox;
 import me.nikl.gamebox.Permissions;
 import me.nikl.gamebox.data.Statistics;
 import me.nikl.gamebox.game.IGameManager;
+import me.nikl.gamebox.util.ItemStackUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
 import java.util.logging.Level;
 
 /**
@@ -32,12 +37,86 @@ public class GameManager implements IGameManager {
     private Map<String,GameRules> gameTypes;
 
 
+    private RandomAccessFile raf;
+
+    private Random random;
+
+    private Map<Integer, ItemStack> cover = new HashMap<>();
+    private Map<Integer, ItemStack> tip = new HashMap<>();
+    private Map<Integer, ItemStack> number = new HashMap<>();
+
+    private boolean problemWhileLoading = false;
+
 
     public GameManager(Main plugin){
         this.plugin = plugin;
         this.lang = plugin.lang;
 
         this.statistics = plugin.gameBox.getStatistics();
+
+        this.random = new Random(System.currentTimeMillis());
+
+
+        File puzzle = new File(plugin.getDataFolder().toString() + File.separatorChar + "puzzles.yml");
+        if(!puzzle.exists()){
+            plugin.saveResource("puzzles.yml", false);
+        }
+
+        try {
+            this.raf  = new RandomAccessFile(puzzle, "r");
+        } catch (FileNotFoundException e) {
+            problemWhileLoading = true;
+            Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " Puzzles can't be found! No games possible");
+            e.printStackTrace();
+        }
+
+        loadItems();
+    }
+
+    private void loadItems() {
+        if(!plugin.getConfig().isConfigurationSection("items.grid")){
+            Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " items.grid section missing in config");
+            problemWhileLoading = true;
+            return;
+        }
+        ConfigurationSection items = plugin.getConfig().getConfigurationSection("items.grid");
+        ItemStack tipItem, coverItem, numberItem;
+        for (int i = 1; i < 10; i++){
+            if(!plugin.getConfig().isConfigurationSection("items.grid." + i)){
+                Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " items.grid." + i + " section missing in config");
+                cover.put(i, new ItemStack(Material.STAINED_GLASS_PANE, 1));
+                tip.put(i, new ItemStack(Material.HARD_CLAY, i));
+                number.put(i, new ItemStack(Material.WOOL, i));
+                continue;
+            }
+
+            tipItem = ItemStackUtil.getItemStack(items.getString(i + "." + "tip" + ".materialData", "HARD_CLAY"));
+            coverItem = ItemStackUtil.getItemStack(items.getString(i + "." + "cover" + ".materialData", "STAINED_GLASS_PANE"));
+            numberItem = ItemStackUtil.getItemStack(items.getString(i + "." + "number" + ".materialData", "WOOL"));
+
+            if(tipItem == null) tipItem = new ItemStack(Material.HARD_CLAY, 1);
+
+
+            if(coverItem == null) coverItem = new ItemStack(Material.STAINED_GLASS_PANE, 1);
+
+            if(numberItem == null) numberItem = new ItemStack(Material.WOOL, 1);
+
+            ItemMeta meta = tipItem.getItemMeta();
+            meta.setDisplayName(GameBox.chatColor(items.getString(i + "." + "tip" + ".displayName", "&l&n" + i)));
+            tipItem.setItemMeta(meta);
+
+            meta = coverItem.getItemMeta();
+            meta.setDisplayName(GameBox.chatColor(items.getString(i + "." + "cover" + ".displayName", "%value%")));
+            coverItem.setItemMeta(meta);
+
+            meta = numberItem.getItemMeta();
+            meta.setDisplayName(GameBox.chatColor(items.getString(i + "." + "number" + ".displayName", "%value%")));
+            numberItem.setItemMeta(meta);
+
+            tip.put(i, tipItem.clone());
+            cover.put(i, coverItem.clone());
+            number.put(i, numberItem.clone());
+        }
     }
 
 
@@ -51,7 +130,8 @@ public class GameManager implements IGameManager {
 
     @Override
     public boolean onInventoryClose(InventoryCloseEvent inventoryCloseEvent) {
-        // ToDo
+        // ToDo:
+        removeFromGame(inventoryCloseEvent.getPlayer().getUniqueId());
 
         return true;
     }
@@ -65,6 +145,8 @@ public class GameManager implements IGameManager {
 
     @Override
     public int startGame(Player[] players, boolean playSounds, String... strings) {
+        if(raf == null) return GameBox.GAME_NOT_STARTED_ERROR;
+
         if (strings.length != 1) {
             Bukkit.getLogger().log(Level.WARNING, " unknown number of arguments to start a game: " + Arrays.asList(strings));
             return GameBox.GAME_NOT_STARTED_ERROR;
@@ -77,12 +159,22 @@ public class GameManager implements IGameManager {
             return GameBox.GAME_NOT_STARTED_ERROR;
         }
 
-        if (!pay(players, rule.getCost())) {
+        if (problemWhileLoading || !pay(players, rule.getCost())) {
             return GameBox.GAME_NOT_ENOUGH_MONEY;
         }
 
-        games.put(players[0].getUniqueId(), new Game(rule, plugin, players[0], playSounds));
-        return GameBox.GAME_STARTED;
+        String puzzle;
+        try {
+            raf.seek(random.nextInt(1465) * 83);
+            puzzle = raf.readLine();
+            games.put(players[0].getUniqueId(), new Game(rule, plugin, players[0], playSounds, puzzle, cover, tip, number));
+            return GameBox.GAME_STARTED;
+
+        } catch (IOException e) {
+            Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " Could not start a game");
+            Bukkit.getConsoleSender().sendMessage(lang.PREFIX + ChatColor.RED + " Could not find the puzzle file");
+            return GameBox.GAME_NOT_STARTED_ERROR;
+        }
     }
 
 
@@ -102,10 +194,10 @@ public class GameManager implements IGameManager {
         if (plugin.isEconEnabled() && !player[0].hasPermission(Permissions.BYPASS_ALL.getPermission()) && !player[0].hasPermission(Permissions.BYPASS_GAME.getPermission(Main.gameID)) && cost > 0.0) {
             if (Main.econ.getBalance(player[0]) >= cost) {
                 Main.econ.withdrawPlayer(player[0], cost);
-                player[0].sendMessage(chatColor(lang.PREFIX + plugin.lang.GAME_PAYED.replaceAll("%cost%", String.valueOf(cost))));
+                player[0].sendMessage(GameBox.chatColor(lang.PREFIX + plugin.lang.GAME_PAYED.replaceAll("%cost%", String.valueOf(cost))));
                 return true;
             } else {
-                player[0].sendMessage(chatColor(lang.PREFIX + plugin.lang.GAME_NOT_ENOUGH_MONEY));
+                player[0].sendMessage(GameBox.chatColor(lang.PREFIX + plugin.lang.GAME_NOT_ENOUGH_MONEY));
                 return false;
             }
         } else {
